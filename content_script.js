14a0ba92e964ed4b067296704bfbe7990f1e7f81
otv0604 <smallassassin@hotmail.co.jp>
2022-12-16 15:17:41 +0900

    rev1 表示項目と表示位置の変更

--- content_script.js
+++ content_script.js
@@ -1,28 +1,37 @@
 var li01 = Array.from(document.getElementsByTagName("td"));
-console.log(li01[23]);
 var hajimene = parseInt(li01[23].textContent.replaceAll(",", ""));
 var takane = parseInt(li01[26].textContent.replaceAll(",", ""));
-
+var yasune = parseInt(li01[29].textContent.replaceAll(",", ""));
+var owarine = parseInt(li01[32].textContent.replaceAll(",", ""));
 var li02 = Array.from(document.getElementsByClassName("floatr"));
-console.log(li02);
-console.log(li02[0].textContent);
 var zen = parseInt(li02[0].textContent.replaceAll(",", ""));
-console.log(zen);
 
-var a1 = ((hajimene - zen) * 100) / zen;
-var a2 = ((takane - zen) * 100) / zen;
 var n = 1;
-var a11 = Math.floor(a1 * Math.pow(10, n)) / Math.pow(10, n);
-var a22 = Math.floor(a2 * Math.pow(10, n)) / Math.pow(10, n);
-console.log(a11);
-console.log(a22);
+var a1 =
+  Math.floor((((hajimene - zen) * 100) / zen) * Math.pow(10, n)) /
+  Math.pow(10, n);
+var a2 =
+  Math.floor((((takane - zen) * 100) / zen) * Math.pow(10, n)) /
+  Math.pow(10, n);
+var a3 =
+  Math.floor((((yasune - zen) * 100) / zen) * Math.pow(10, n)) /
+  Math.pow(10, n);
+var a4 =
+  Math.floor((((owarine - zen) * 100) / zen) * Math.pow(10, n)) /
+  Math.pow(10, n);
 
-const h = document.getElementsByTagName("th");
+const h = document.getElementsByTagName("tr");
 console.log(h);
-const newh1 = document.createElement("td");
-newh1.textContent = a11 + "%";
-h[9].appendChild(newh1);
 
+const newh1 = document.createElement("td");
+newh1.textContent = a1 + "%";
+h[7].appendChild(newh1);
 const newh2 = document.createElement("td");
-newh2.textContent = a22 + "%";
-h[10].appendChild(newh2);
+newh2.textContent = a2 + "%";
+h[8].appendChild(newh2);
+const newh3 = document.createElement("td");
+newh3.textContent = a3 + "%";
+h[9].appendChild(newh3);
+const newh4 = document.createElement("td");
+newh4.textContent = a4 + "%";
+h[10].appendChild(newh4);
