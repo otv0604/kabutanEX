c05f1039f03dff05ec72f166228a808f813b93d7
otv0604 <smallassassin@hotmail.co.jp>
2022-12-28 23:57:54 +0900

    rev1.4 for文で簡略化 425スレ564レス

--- content_script.js
+++ content_script.js
@@ -1,10 +1,21 @@
-var li01 = Array.from(document.getElementsByTagName("td"));
-var hajimene = parseInt(li01[23].textContent.replaceAll(",", ""));
-var takane = parseInt(li01[26].textContent.replaceAll(",", ""));
-var yasune = parseInt(li01[29].textContent.replaceAll(",", ""));
-var owarine = parseInt(li01[32].textContent.replaceAll(",", ""));
-var li02 = Array.from(document.getElementsByClassName("floatr"));
-var zen = parseInt(li02[0].textContent.replaceAll(",", ""));
+// 前日終値取得
+var zen = parseInt(
+  Array.from(
+    document.getElementsByClassName("floatr"))[0].textContent.replaceAll(",", "")
+  );
+
+// thを限定している
+const kl = document.getElementById("kobetsu_left");
+const tb = kl.getElementsByTagName("tbody")[0];
+const th = tb.getElementsByTagName("th");
+
+// 425スレ564レス
+let ohlc = [];
+for (let i of th) {
+  ohlc.push(
+    calcRatio(parseInt(i.nextElementSibling.textContent.replaceAll(",", "")),zen)
+  );
+}
 
 // 424スレ469レス
 function calcRatio(ohlc, zenzitu) {
@@ -15,31 +26,25 @@ function calcRatio(ohlc, zenzitu) {
   );
 }
 
-const a = [
-  calcRatio(hajimene, zen),
-  calcRatio(takane, zen),
-  calcRatio(yasune, zen),
-  calcRatio(owarine, zen),
-];
-
+// 挿入する位置のエレメントを取得
 const h = document.getElementsByTagName("tr");
-console.log(h);
 
+// 条件ごとにフォント変更
 let arr = [];
 for (let i = 0; i < 4; i++) {
   arr[i] = document.createElement("td");
-  if (a[i] <= -5) {
+  if (ohlc[i] <= -5) {
     arr[i].style.color = "blue";
     arr[i].style.fontWeight = "bold";
-  } else if (a[i] < 0) {
+  } else if (ohlc[i] < 0) {
     arr[i].style.color = "blue";
-  } else if (a[i] == 0) {
-  } else if (a[i] < 5) {
+  } else if (ohlc[i] == 0) {
+  } else if (ohlc[i] < 5) {
     arr[i].style.color = "red";
   } else {
     arr[i].style.color = "red";
     arr[i].style.fontWeight = "bold";
   }
-  arr[i].textContent = a[i] + "%";
+  arr[i].textContent = ohlc[i] + "%";
   h[i + 7].appendChild(arr[i]);
 }
